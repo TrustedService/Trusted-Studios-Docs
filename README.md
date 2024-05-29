552be0012c907db6b4515dd3ea14fd30031d2b5b
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2024-05-29 11:25:12 +0200

    sync repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Trusted Studios official Docs
+
+This is the official Trusted Studios docs using the [Nextra Docs Template](https://nextra.site). Feel free to submit pull requests if you want to help us improve the docs.
---
+++ components/Badges.tsx
@@ -0,0 +1,25 @@
+import React from "react"; // Import React
+
+type BadgeProps = {
+    free: boolean;
+};
+
+// Use the correct type for the props parameter and provide a default value for "tp"
+const Badge: React.FC<BadgeProps> = ({ free = false }) => {
+    // You can remove the conditional check for "tp" since it's not used
+    return (
+        <div className="flex w-32 flex-col items-center justify-center gap-2">
+            {free ? (
+                <span className="bg-green-700 text-white text-xs font-medium mr-2 px-2.5 py-0.5 rounded border border-dark">
+                    FREE
+                </span>
+            ) : (
+                <span className="bg-red-700 text-white text-xs font-medium mr-2 px-2.5 py-0.5 rounded border border-dark">
+                    PAID
+                </span>
+            )}
+        </div>
+    );
+};
+
+export default Badge;
