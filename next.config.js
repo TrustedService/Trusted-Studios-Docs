cf8e6cc88bccd95d2bd5fc805dc964dcdd7027f2
Thomas Holtz <thomasholtz.pr@gmail.com>
2024-10-18 22:58:16 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  latex: true
+  latex: true,
+    /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/docs",
 })
 
 module.exports = {
@@ -9,5 +22,4 @@ module.exports = {
   images: {
     unoptimized: true,
   },
-  basePath: "/Trusted-Studios-Docs",
 }
