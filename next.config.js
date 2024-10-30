a1498b4a5ddb5c9eae362a83d7657581aacb43a6
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2024-10-30 09:21:12 +0100

    adjusting next.js settings for githu site deployment

--- next.config.js
+++ next.config.js
@@ -1,25 +1,13 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  latex: true,
-    /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
-  output: "export",
-
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: '/Trusted-Studios-Docs',
 })
 
 module.exports = {
   ...withNextra(),
+  rewrites: null,
+  output: 'export',
   images: {
     unoptimized: true,
   },
-}
+};
