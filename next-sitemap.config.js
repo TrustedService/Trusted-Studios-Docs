3f0d1ddf8e3899bd95ef683a2f69cf090d9484d5
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2024-10-30 09:44:06 +0100

    added static build with next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://docs.trusted-studios.eu/",
+    generateRobotsTxt: true,
+    generateIndexSitemap: false,
+    outDir: "./out",
+};
